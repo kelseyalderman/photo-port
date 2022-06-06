a5b0eb82ac5414cf4eac542e0f956d551f185835
Kelsey Alderman <kelseyalderman@gmail.com>
2022-06-05 20:51:40 -0700

    test Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -41,7 +41,7 @@ function ContactForm() {
   // JSX
   return (
     <section>
-      <h1>Contact Me</h1>
+      <h1 data-testid="h1tag">Contact Me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -75,7 +75,9 @@ function ContactForm() {
             <p className="error-text">{errorMessage}</p>
           </div>
         )}
-        <button type="submit">Sumbit</button>
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
